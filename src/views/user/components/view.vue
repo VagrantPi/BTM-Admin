739293b9d30ce9a99f09042ea054e7f96de1f4ea
VagrantPi <xup6u06m4@gmail.com>
2025-04-08 15:11:44 +0800

    feat: 基本資料頁面補資料

--- src/views/user/components/view.vue
+++ src/views/user/components/view.vue
@@ -18,11 +18,12 @@
       <el-col :span="16">
         <div class="label">姓名：{{ info.name }}</div>
         <div class="label">身分證號：{{ info.id_number }}</div>
-        <div class="label">出身年月日：{{ info.birthday }}</div>
+        <div class="label">出身年月日：{{ info.birthday }} ({{ calculateAge(info.birthday) }} 歲)</div>
         <div class="label">地址：{{ info.address }}</div>
         <div class="label">職業：{{ info.occupation }}</div>
         <div class="label">手機：{{ info.phone_number }}</div>
         <div class="label">Email：{{ info.email }}</div>
+        <div class="label">Customer ID：{{ customer_id }}</div>
       </el-col>
       <el-col :span="8">
         <div class="block">
@@ -144,6 +145,34 @@ export default {
       const selfieImgResponse = await getSumsubImage({ inspection_id: this.info.inspection_id, image_id: selfieImgId }, this.$store.getters.token)
       const selfieImgBlob = new Blob([selfieImgResponse.data], { type: 'image/jpeg' })
       this.urls.selfie_img = URL.createObjectURL(selfieImgBlob)
+    },
+    calculateAge(birthDateStr) {
+      // 檢查輸入格式
+      if (!/^\d{4}-\d{2}-\d{2}$/.test(birthDateStr)) {
+        return 0
+      }
+
+      // 將字串轉換為日期對象
+      const birthDate = new Date(birthDateStr)
+
+      // 檢查日期是否有效
+      if (isNaN(birthDate.getTime())) {
+        throw new Error('無效的日期')
+      }
+
+      // 獲取當前日期
+      const today = new Date()
+
+      // 計算年齡差異
+      let age = today.getFullYear() - birthDate.getFullYear()
+
+      // 檢查今年的生日是否已經過了
+      const monthDiff = today.getMonth() - birthDate.getMonth()
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age-- // 如果今年生日還沒到，年齡減1
+      }
+
+      return age
     }
   }
 }
